ab8128d07a1829f49eab6eafd59cbd4466b64228
phenry <phenry@asgat.net>
2018-08-20 04:36:54 +0200

    now with the health-check URL as environment variable in Dockerfile.

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,39 +1,67 @@
 import os
 from flask import Flask
 from healthcheck import HealthCheck, EnvironmentDump
+from environs import Env
+import json
+import logging
 import requests
+import sys
+
+
+env = Env()
+env.read_env()
+urls = env.json("URL")
+#urls = json.loads(os.environ["URL"])
+
 
 app = Flask(__name__)
 
+
+def setup_logging():
+  if not app.debug:
+    # In production mode, add log handler to sys.stderr.
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+ 
+setup_logging()
+
+for url in urls:
+  app.logger.info("URL to check: %s" % url)
+
+
 health = HealthCheck(app, "/check")
+#envdump = EnvironmentDump(app, "/environment")
 envdump = EnvironmentDump(app, "/environment", include_process=False, include_config=False)
 
 
-# Test URL
-# http://httpbin.org/status/200
-# http://httpbin.org/status/500
-
 def check_1():
-  URL1 = os.environ.get('URL1', "http://httpbin.org/status/200")
-  r = requests.get(URL1)
-  if r.status_code == 200:
-    return True, "OK " + URL1
-  else:
-    return False, "KO " + URL1
+  url = urls[0]
+  r = requests.get(url)
+  if r.status_code == 200: return True, "OK %s" % url
+  else: return False, "KO %s" % url
 
 def check_2():
-  URL2 = os.environ.get('URL2', "http://httpbin.org/status/500")
-  r = requests.get(URL2)
-  if r.status_code == 200:
-    return True, "OK " + URL2
-  else:
-    return False, "KO " + URL2
+  url = urls[1]
+  r = requests.get(url)
+  if r.status_code == 200: return True, "OK %s" % url
+  else: return False, "KO %s" % url
 
-health.add_check(check_1)
-health.add_check(check_2)
+def check_3():
+  url = urls[2]
+  r = requests.get(url)
+  if r.status_code == 200: return True, "OK %s" % url
+  else: return False, "KO %s" % url
 
 
-# @app.route("/")
-# @app.route("/check")
-# def root():
-#     return "Hello World!"   
+if len(urls)>0: health.add_check(check_1) 
+if len(urls)>1: health.add_check(check_2) 
+if len(urls)>2: health.add_check(check_3) 
+
+
+#@app.route("/")
+#def root():
+#  return "Hello World!"
+
--- web/run.py
+++ web/run.py
@@ -1,8 +1,8 @@
-#DEBUG = True
 import os
 from app import app
- 
+
+#app.config['DEBUG'] = os.environ.get('DEBUG', False)
+#app.config['DEBUG'] = True
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    #app.run(host='127.0.0.1', port=port)
-    app.run(host='0.0.0.0', port=port)
+  app.run(host='127.0.0.1', port=int(os.environ.get('PORT', 5000)))
